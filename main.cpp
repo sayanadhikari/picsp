b036e62fb71e8db2c9cf3d0a336d33953bd5fc89
Sayan Adhikari <sayanadhikari207@gmail.com>
2020-05-09 20:44:04 +0200

    test iniparser

--- main.cpp
+++ main.cpp
@@ -71,7 +71,7 @@ int nParticlesE = 50000; // Number of simulation electrons
 
 const int NC =  200;             // Total number of cells
 int nTimeSteps = 1000;          // Total time steps (default)
-
+double mass_ion;  // Ion mass
 
 /* Class Domain: Hold the domain parameters*/
 class Domain
