c67224e7f7cac40c53dcbe8cb67ef57569a8606f
Sayan Adhikari <sayanadhikari207@gmail.com>
2020-06-10 11:14:37 +0200

    update

--- scripts/phase_space_animate_using_func.py
+++ scripts/phase_space_animate_using_func.py
@@ -1,17 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
+import matplotlib.animation as animation
 from pylab import *
 
+nTimeSteps = 1000
 
-# numerical data file
-filename1="../output/phase_space/i20000.dat"
-filename2="../output/phase_space/e20000.dat"
 
-x1,v1 = np.loadtxt(filename1, unpack=True)
-x2,v2 = np.loadtxt(filename2, unpack=True)
-# disp(data_act.shape)
-
+DATA = []
 # DEFAULT VALUES
 eps_0   = 8.85418782e-12
 q       = 1.602e-19 # charge
@@ -24,17 +19,31 @@ TiSI    = sqrt(2*(Ti*q)/M_i)
 gamma   = 1.67
 KB      = 1.38064852E-23
 n_0     = 1e16
-Omega_pe = np.sqrt((n_0*q*q)/(M_e*eps_0))
-Omega_pi = np.sqrt((n_0*q*q)/(M_i*eps_0))
+
 DL       = np.sqrt((TeSI*KB*eps_0)/(n_0*q*q))
 
+# numerical data file
+DIR ="../output/phase_space"
 
+for i in range(0,nTimeSteps,50):
+    filename1=DIR+'/i%d'%i+'.dat'
+    filename2=DIR+'/e%d'%i+'.dat'
+
+    x1,v1 = np.loadtxt(filename1, unpack=True)
+    x2,v2 = np.loadtxt(filename2, unpack=True)
+ 
+ DATA = DATA[start_index:,:,:]
+ ani=animation.FuncAnimation(fig,animate,len(DATA[:,0,0]),interval=interval*1e+3,blit=False)
+ 
+ 
 figure(1)
 plt.scatter(x1/DL,v1,s=1,marker='.')
 plt.xlabel("$x/\\lambda_D$")
 plt.ylabel("$V_x$")
 plt.title('Ion Phase Space')
 plt.tight_layout()
+plt.pause(0.1)
+plt.clf()
 
 
 figure(2)
@@ -43,6 +52,6 @@ plt.xlabel("$x/\\lambda_D$")
 plt.ylabel("$V_x$")
 plt.title('Electron Phase Space')
 plt.tight_layout()
-
-plt.show()
+plt.pause(0.1)
+plt.clf()
 
